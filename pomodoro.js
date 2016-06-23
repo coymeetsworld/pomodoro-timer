ca042ed7d673152a8b69024515731fc125ad38e9
Coy Sanders <coymeetsworld@gmail.com>
2016-06-23 02:18:23 -0700

    Added notification feature and audible cue for when timer ends.

--- pomodoro.js
+++ pomodoro.js
@@ -1,11 +1,53 @@
 $(document).ready(function() {
 
   var SECONDS_IN_MINUTE = 60;
+  var DEFAULT_POMODORO_LENGTH = 25;
+  var DEFAULT_BREAK_LENGTH = 5;
+  var DEFAULT_NUM_POMODOROS = 4;
+  var chime = new Audio('media/ding.mp3');
+
   /* Converts a time string (MM:SS) to number of seconds. Used for startTimer function. */
   function convertTimeToSeconds(time_string) {
     var time_array = time_string.split(":");
     var seconds = (SECONDS_IN_MINUTE * (+time_array[0]) + (+time_array[1]));
     return seconds;
+  }
+
+  /* Used to send notifications to the user for when a pomodoro or break finishes. This will help signal the user if they are not currently on the pomodoro page. */
+  function notifyUser(state) {
+    console.log("Notified: " + state);
+    var msg = "";
+    if (state == 'SESSION') {
+      msg = "Session " + numPomodorosCompleted + " completed!";
+    } else if (state == 'BREAK') {
+      msg = "Break is over, starting next pomodoro now.";
+    } else if (state == 'COMPLETED') {
+      msg = "Pomodoros completed!";
+    }
+    console.log("Notification: " + Notification.permission);
+    if (!("Notification" in window)) {
+      //The browser doesn't support desktop notification
+      alert(msg);
+    }
+    else if (Notification.permission === "granted") { // Checks if notif permissions granted already
+      var notification = new Notification(msg);
+    }
+    else if (Notification.permission !== 'denied' || Notification.permission === "default") { // Lets ask user for permission
+      Notification.requestPermission(function (permission) {
+        // If the user accepts, let's create a notification
+        if (permission === "granted") {
+          var notification = new Notification(msg);
+        }
+      });
+    }
+  }Notification.requestPermission().then(function(result) {
+    console.log(result);
+  });function spawnNotification(theBody,theIcon,theTitle) {
+    var options = {
+      body: theBody,
+      icon: theIcon
+    }
+    var n = new Notification(theTitle,options);
   }
 
   /* Formats number of seconds to a time string (MM:SS). Used in display of timer. */
@@ -18,18 +60,21 @@ $(document).ready(function() {
   }
 
 
-  var pomodoroDuration = 25*SECONDS_IN_MINUTE; /* Default value */
+  var pomodoroDuration = DEFAULT_POMODORO_LENGTH*SECONDS_IN_MINUTE; /* Default value */
   var pomodoroDurationFormatted = formatTimeLeft(pomodoroDuration);
   $("#time_left").html("<p>" + pomodoroDurationFormatted + "</p>");
   $("#pLength").html("<p>" + pomodoroDurationFormatted + "</p>");
 
-  var breakDuration = 5*SECONDS_IN_MINUTE; /* Default value */
+  var breakDuration = DEFAULT_BREAK_LENGTH*SECONDS_IN_MINUTE; /* Default value */
   var breakDurationFormatted = formatTimeLeft(breakDuration);
   $("#bLength").html("<p>" + breakDurationFormatted + "</p>");
 
-  var numPomodoros = 1;
+  var numPomodoros = DEFAULT_NUM_POMODOROS;
   var numPomodorosCompleted = 0;
   $("#numPomodoros").html("<p>" + numPomodoros + "</p>");
+  for (var i = 0; i < DEFAULT_NUM_POMODOROS; i++) {
+    $("#row_pomodoros").append("<img class=\"pomodoro_pending\" src=\"images/tomato1.svg\"/>");
+  }
 
   var mode = "Paused";
   var prev_mode = "Session"; /* Used to keep track of what mode it is prior to pausing (i.e. when resumed, checks if its on Break or Session). By default start as Session. */
@@ -79,7 +124,7 @@ $(document).ready(function() {
     clearInterval(myInterval);
     mode = "Completed";
     $("#startTimerButton").html("Start");
-    alert("DONE!");
+    notifyUser("COMPLETED");
     $("#row_menu").slideDown(600);
     $("#row_pomodoros").animate({bottom: '50px'}, 600);
     return true;
@@ -105,20 +150,24 @@ $(document).ready(function() {
 
 
       if (timeLeft <= 0) {
-
+        chime.play();
         if (mode == "Session") {
           incrementPomodorosCompleted();
+
           if (!sessionsCompleted()) {
+            notifyUser("SESSION");
             mode = "Break!";
             durationInSeconds = breakDuration;
           }
         } else {
+          notifyUser("BREAK");
           mode = "Session";
           durationInSeconds = pomodoroDuration;
         }
         $("#mode").html("<p>Mode: " + mode + "</p>");
         start = Date.now();
-        start.setSeconds(start.getSeconds() + 2);
+        /*console.log("THIS: " + start);
+        start.setSeconds(start.getSeconds() + 2);*/
       }
 
     };
@@ -132,6 +181,7 @@ $(document).ready(function() {
 
   /* Depending on the current state, will either start or pause the session. If started, buttons to change the pomodoro/break lengths will be disabled. The opposite is true if the button is pressed to pause the session. */
   $("#startTimerButton").click(function() {
+
     if (mode == "Paused") {
       $("#row_menu").slideUp(600);
       $("#row_pomodoros").animate({bottom: '0px'}, 600);
